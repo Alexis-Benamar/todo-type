da1197f83b418ab090a87e4a779b82e570028778
a.benamar <a.benamar@attineos.com>
2020-05-27 10:45:03 +0200

    removed unused types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,12 +1,6 @@
-type AddTodoHandlerType = (state, newTodo: TodoType) => void
-
-type RemoveTodoHandlerType = (state, deletedTodo: number) => void
-
 type TodoType = {
     title: string
     text: string
     idx: number
     complete: boolean
 }
-
-type ToggleTodoType = (state, selectedTodo: TodoType) => void
