88201982fcb4d5ed97886e5ec7f8b4ec70de54cf
a.benamar <a.benamar@attineos.com>
2020-05-29 15:35:01 +0200

    set default lang to EN

--- src/i18n.ts
+++ src/i18n.ts
@@ -9,9 +9,8 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'fr',
-    lng: 'fr',
-    debug: true,
+    fallbackLng: 'en',
+    lng: 'en',
     defaultNS: 'translation',
 
     interpolation: {
